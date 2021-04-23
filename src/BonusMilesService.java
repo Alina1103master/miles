40762c8b9ad7d42a99e323918159d003b50a4f49
Alina <77508351+Alina1103master@users.noreply.github.com>
2021-04-23 15:42:08 +0300

    initial commit

---
+++ src/BonusMilesService.java
@@ -0,0 +1,8 @@
+public class BonusMilesService {
+    public int calculate(int cost) {
+        int part = 20;
+        int price = 10000;
+        int miles = cost / part;
+        return cost / part;
+    }
+}
